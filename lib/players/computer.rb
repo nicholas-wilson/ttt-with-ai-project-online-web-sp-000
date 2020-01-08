9cf51e79779ef46a9f388124abcabfbe32497ef8
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-08 07:47:40 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,5 +1,15 @@
 class Players::Computer < Player
   attr_accessor :marker
+  WIN_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ]
 
   def move(board)
     if board.turn_count == 0 || board.turn_count == 1
@@ -26,6 +36,6 @@ class Players::Computer < Player
 
   def one_off_win?(board)
     one_off = false
-
+    if
   end
 end
