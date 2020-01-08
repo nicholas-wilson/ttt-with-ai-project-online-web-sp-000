536b772265674f8a4f3cdfc0dd0046481c2993a8
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-08 08:29:05 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,13 @@ require 'bundler'
 Bundler.require
 
 require_all 'lib'
+
+puts "Please enter number of players 0, 1, or 2."
+input = gets.chomp
+until input == "0" || input == "1" || input == "2"
+  puts "Please enter number of players 0-2."
+  input = gets.chomp
+end
+the_game = nil
+if input == "0"
+  the_game = Game.new(Players::Computer("X"), Players::Computer("O"))
